56fdb0c0d47e3a45b819c0964985c523646b635a
rashuno <rashuno@gmail.com>
2013-08-01 00:51:14 -0400

    Updates

--- SandBox/js/app.js
+++ SandBox/js/app.js
@@ -2,13 +2,16 @@
  
 /* --allow-file-access-from-files    */
 /* --disable-web-security  */
-
-
-
+ 
 angular.module('myApp', [ 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'  ])
-  .config( ['$routeProvider', function ($routeProvider) {
+  .config( ['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
     $routeProvider.when('/Home', {templateUrl: 'partials/HomeTab.html', controller:  'HomeCtrl' });
     $routeProvider.when('/MyDraft', {templateUrl: 'partials/MyDraftTab.html', controller: 'DraftCtrl' });
     $routeProvider.when('/ResearchPlayers', {templateUrl: 'partials/ResearchPlayersTab.html', controller: 'ResearchCtrl'});
     $routeProvider.otherwise({redirectTo: '/Home' });
-  }]);
+	
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }]
+   
+  );
+   
--- SandBox/js/controllers.js
+++ SandBox/js/controllers.js
@@ -26,26 +26,61 @@ var homeCtrl = myApp.controller('HomeCtrl', [function($scope) {
 var researchCtrl = myApp.controller('ResearchCtrl', ['$scope', '$http', function($scope, $http) {
 
 	/* $scope.url = "http://api.espn.com/v1/sports/football/nfl/teams/?apikey=6w3k6rxsnm4a8g6sn9k9rq8s";  
+		http://api.espn.com/v1/sports/football/nfl/athletes/?offset=0&apikey=6w3k6rxsnm4a8g6sn9k9rq8s   OFFSET
 		data/teams.json
 
+
+		nfldata.com
+		api key = 44d7bd92-a369-4a22-b188-3c12303890c2
+		GET TEAMS
+		http://api.nfldata.apiphany.com/trial/JSON/Teams/2013REG?key=44d7bd92-a369-4a22-b188-3c12303890c2
+		"City":"Arizona","Conference":"NFC","Division":"West","FullName":"Arizona Cardinals","Key":"ARI","Name":"Cardinals"
+
+		GET TEAM ROSTER n DEPTH CHART
+		http://api.nfldata.apiphany.com/trial/{format}/Players/{team}?key=<Your developer key>
+
+		GET PLAYER STATS and NEWS
+		http://api.nfldata.apiphany.com/trial/{format}/Player/{playerid}?key=<Your developer key>
 	*/
-	$scope.url = 'data/teams.json';
+	
+	
+
+	/* Get Teams Data JSON From ESPN Dev API  */
+	
 	$scope.newMessage = "";
 	
 	 var sports = {};
 	 var leagues = {};
 	$scope.teams = {};
+	$scope.athletes = {};
+	$scope.IsTeamSelected = 'false';
 	
-	$scope.add = function() {
+	$scope.addNFLTeams = function() {
+		$http.defaults.useXDomain = true;
+		$scope.url = 'http://api.nfldata.apiphany.com/trial/JSON/Teams/2013REG?key=44d7bd92-a369-4a22-b188-3c12303890c2';
 		$http.get($scope.url).then(function(response) {
 		
+			/*
 				sports = response.data.sports;
 				leagues = sports[0].leagues;
-				
-				$scope.teams = leagues[0].teams;
-				$scope.newMessage = $scope.teams[1].nickname;
+			*/	
+				$scope.teams = response.data; /* leagues[0].teams; */
 				
 					
+		});
+	};
+
+
+	$scope.addNFLPlayers = function() {
+		$http.defaults.useXDomain = true;
+		$scope.url = 'http://api.espn.com/v1/sports/football/nfl/athletes/?apikey=6w3k6rxsnm4a8g6sn9k9rq8s';
+		$http.get($scope.url).then(function(response){
+
+			sports = response.data.sports;
+			leagues = sports[0].leagues;
+
+			$scope.athletes = leagues[0].athletes;
+
 		});
 	};
 	
